71fb063ee4d13e60f1743f8a96bbbce0d3c22f1e
xxqqwua <xxqqwua@gmail.com>
2025-06-08 14:06:50 +0300

    add: retrieve data from clipboard add: paste corrected text to clipboard add: paste corrected text from clipboard

---
+++ main_module/clipboard_handler.py
@@ -0,0 +1,22 @@
+import pyperclip
+import win32clipboard
+
+class ClipboardHandler:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_clipboard():  # get clipboard data
+        win32clipboard.OpenClipboard()
+        data = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
+
+        return data
+
+    @staticmethod
+    def send_corrected(text):  # send corrected text to clipboard
+        pyperclip.copy(text)
+
+    @staticmethod
+    def paste_corrected():  # paste corrected text from clipboard
+        pyperclip.paste()
