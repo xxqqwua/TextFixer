775e647b3338b544604e97298a6703bb90ca3efd
xxqqwua <xxqqwua@gmail.com>
2025-06-08 16:06:38 +0300

    fix: wrong language code led to incorrect translation add: return text if language code does not match

--- main_module/corrector.py
+++ main_module/corrector.py
@@ -6,10 +6,28 @@ from language_tool_python import utils
 
 class Corrector:
     def __init__(self):
-        pass
+        self.LANG_CODE_MAP = {
+            'en': 'en-US',  # English - default US
+            'de': 'de-DE',  # German - region specification
+            'it': 'it-IT',  # Italian
+            'pl': 'pl-PL',  # Polish
+            'pt': 'pt-PT',  # Portuguese - default EU
+            'ro': 'ro-RO',  # Romanian
+            'ru': 'ru-RU',  # Russian
+            'sk': 'sk-SK',  # Slovakian
+            'sl': 'sl-SI',  # Slovenian
+            'uk': 'uk-UA',  # Ukrainian
+            'zh-cn': 'zh-CN',  # Simplified Chinese
+            'zh-tw': 'zh-TW',  # Traditional Chinese
+            'ja': 'ja-JP',  # Japanese
+        }
 
-    @staticmethod
-    async def correct_text(text, lang_code):
+    async def correct_text(self, text, lang_code):
+        lang_code = self.LANG_CODE_MAP.get(lang_code)
+
+        if not lang_code:
+            return text
+
         def run_check():
             tool = language_tool_python.LanguageTool(lang_code)
             matches = tool.check(text)
